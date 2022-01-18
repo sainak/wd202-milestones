f8bc7f6313d19118fe38d2cc54957aa2d30026cd
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-18 23:36:03 +0530

    added starter views

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Add all your views here
+    path("tasks/", views.task_view),
+    path("add-task/", views.add_task_view),
+    path("delete-task/<int:index>/", views.delete_task_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,21 @@
-# Add all your views here
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+tasks = []
+
+
+def task_view(request):
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def add_task_view(request):
+    task_value = request.GET.get("task")
+    if task_value:
+        tasks.append(task_value)
+    return HttpResponseRedirect("/tasks")
+
+
+def delete_task_view(request, index):
+    if index:
+        del tasks[index - 1]
+    return HttpResponseRedirect("/tasks")
