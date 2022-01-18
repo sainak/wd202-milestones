c3a9de6412293de8df5f8665ad83a43ccf059a2e
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-18 23:57:52 +0530

    try for bounty points

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,6 +6,7 @@ from tasks import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("all_tasks/", views.all_tasks_view),
     path("tasks/", views.task_view),
     path("completed_tasks/", views.completed_tasks_view),
     path("add-task/", views.add_task_view),
--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,17 @@ tasks = []
 completed_tasks = []
 
 
+def all_tasks_view(request):
+    return render(
+        request,
+        "all_tasks.html",
+        {
+            "tasks": tasks,
+            "completed_tasks": completed_tasks,
+        },
+    )
+
+
 def task_view(request):
     return render(request, "tasks.html", {"tasks": tasks})
 
@@ -17,16 +28,16 @@ def add_task_view(request):
     task_value = request.GET.get("task")
     if task_value:
         tasks.append(task_value)
-    return HttpResponseRedirect("/tasks")
+    return HttpResponseRedirect("/all_tasks")
 
 
 def delete_task_view(request, index):
     if index:
         del tasks[index - 1]
-    return HttpResponseRedirect("/tasks")
+    return HttpResponseRedirect("/all_tasks")
 
 
 def complete_task_view(request, index):
     if index:
         completed_tasks.append(tasks.pop(index - 1))
-    return HttpResponseRedirect("/tasks")
+    return HttpResponseRedirect("/all_tasks")
