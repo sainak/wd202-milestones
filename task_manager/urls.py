e626ff31f80776fb86691840c7d48196a36de2d8
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-18 23:48:30 +0530

    solution

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,6 +7,8 @@ from tasks import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", views.task_view),
+    path("completed_tasks/", views.completed_tasks_view),
     path("add-task/", views.add_task_view),
     path("delete-task/<int:index>/", views.delete_task_view),
+    path("complete_task/<int:index>/", views.complete_task_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 tasks = []
+completed_tasks = []
 
 
 def task_view(request):
     return render(request, "tasks.html", {"tasks": tasks})
+
+
+def completed_tasks_view(request):
+    return render(request, "completed_tasks.html", {"completed_tasks": completed_tasks})
 
 
 def add_task_view(request):
@@ -18,4 +23,10 @@ def add_task_view(request):
 def delete_task_view(request, index):
     if index:
         del tasks[index - 1]
+    return HttpResponseRedirect("/tasks")
+
+
+def complete_task_view(request, index):
+    if index:
+        completed_tasks.append(tasks.pop(index - 1))
     return HttpResponseRedirect("/tasks")
