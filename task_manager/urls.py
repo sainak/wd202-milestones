05369e2bd084b17c7c49f08e2a3c2c8499818197
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-19 12:46:19 +0530

    solution

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks import views
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("all_tasks/", views.all_tasks_view),
+    path("tasks/", views.task_view),
+    path("completed_tasks/", views.completed_tasks_view),
+    path("add-task/", views.add_task_view),
+    path("delete-task/<int:pk>/", views.delete_task_view),
+    path("complete_task/<int:pk>/", views.complete_task_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,58 @@
-# Add your Views Here
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+from .models import Task
+
+
+tasks = []
+completed_tasks = []
+
+
+def all_tasks_view(request):
+    tasks = Task.objects.all().filter(deleted=False)
+    completed_tasks = tasks.filter(completed=True)
+    return render(
+        request,
+        "all_tasks.html",
+        {
+            "tasks": tasks,
+            "completed_tasks": completed_tasks,
+        },
+    )
+
+
+def task_view(request):
+    search_task = request.GET.get("search")
+    tasks = Task.objects.all().filter(deleted=False, completed=False)
+    if search_task:
+        tasks = tasks.filter(title__icontains=search_task)
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def completed_tasks_view(request):
+    completed_tasks = Task.objects.filter(completed=True, deleted=False)
+    return render(request, "completed_tasks.html", {"completed_tasks": completed_tasks})
+
+
+def add_task_view(request):
+    task_value = request.GET.get("task")
+    task = Task(title=task_value)
+    if request.user.is_authenticated:
+        task.user = request.user
+    task.save()
+    return HttpResponseRedirect("/tasks")
+
+
+def delete_task_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.deleted = True
+    task.save()
+    return HttpResponseRedirect("/tasks")
+
+
+def complete_task_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.completed = True
+    task.save()
+    return HttpResponseRedirect("/tasks")
