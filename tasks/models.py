f97c0103860169494c930a37903c2f9e32b46cda
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-21 23:08:33 +0530

    make description nullable

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
