ec370e068ec4309be0a81b3c844ff3604d1e52df
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-22 18:03:00 +0530

    detailed task admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from tasks.models import Task
 
-# Register your models here.
 
-
-admin.sites.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_date", "completed", "deleted")
+    list_display_links = ("id", "title")
+    list_filter = ("created_date",)
+    search_fields = ("title", "description")
+    readonly_fields = ("created_date",)
