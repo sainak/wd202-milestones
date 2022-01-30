266eae5d07d723666c867b4bcbb01705d2235abf
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-30 12:10:27 +0530

    added celery

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -138,6 +138,10 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LOGGING = {
     "version": 1,
