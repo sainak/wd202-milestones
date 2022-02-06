5a8ddb610e0650b19b616f24ecd30893c1ca1c18
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-02-06 23:49:46 +0530

    lambda: settings.INSTALLED_APPS is not required
    https://github.com/celery/celery/issues/3341#issuecomment-235408048

--- task_manager/celery.py
+++ task_manager/celery.py
@@ -1,9 +1,8 @@
 import os
 
 from celery import Celery
-from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
 app = Celery("task_manager")
 app.config_from_object("django.conf:settings")
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
