b459d1ecf3da974b92c17ae3d32792c8cb40c8a9
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-02-14 00:16:31 +0530

    added tasks api integration tests

--- tasks/tests/test_integration_api.py
+++ tasks/tests/test_integration_api.py
@@ -13,7 +13,7 @@ class TaskApiIntegrationTests(TestCase):
             username="testuser", email="test@test.com", password="testpassword"
         )
 
-    def test_get_task_list_view(self):
+    def test_get_task_list(self):
         Task.objects.create(
             title="Test Task",
             owner=self.user,
@@ -25,7 +25,7 @@ class TaskApiIntegrationTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
 
-    def test_get_task_detail_view(self):
+    def test_get_task_detail(self):
         Task.objects.create(
             title="Test Task",
             owner=self.user,
@@ -36,3 +36,69 @@ class TaskApiIntegrationTests(TestCase):
         response = self.client.get(reverse("api-task-detail", kwargs={"pk": 1}))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["title"], "Test Task")
+
+    def test_create_new_task(self):
+        self.client.login(username="testuser", password="testpassword")
+        response = self.client.post(
+            reverse("api-task-list"),
+            {
+                "title": "Test Task",
+                "description": "Test Description",
+                "priority": 1,
+            },
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Task.objects.count(), 1)
+
+    def test_update_task(self):
+        task = Task.objects.create(
+            title="Test Task",
+            owner=self.user,
+            description="Test Description",
+            priority=1,
+        )
+        self.client.login(username="testuser", password="testpassword")
+        response = self.client.put(
+            reverse("api-task-detail", kwargs={"pk": task.pk}),
+            {"title": "Updated Task"},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["title"], "Updated Task")
+
+    def test_delete_task(self):
+        task = Task.objects.create(
+            title="Test Task",
+            owner=self.user,
+            description="Test Description",
+            priority=1,
+        )
+        self.client.login(username="testuser", password="testpassword")
+        response = self.client.delete(
+            reverse("api-task-detail", kwargs={"pk": task.pk})
+        )
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Task.objects.count(), 0)
+
+
+class TaskHistoryApiIntegrationTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            username="testuser", email="test@test.com", password="testpassword"
+        )
+
+    def test_get_task_history(self):
+        task = Task.objects.create(
+            title="Test Task",
+            owner=self.user,
+            description="Test Description",
+            priority=1,
+        )
+        task.status = "COMPLETED"
+        task.save()
+        self.client.login(username="testuser", password="testpassword")
+        response = self.client.get(
+            reverse("api-task-history-list", kwargs={"task_pk": 1})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
